6d67a510b8e87e663d01dfa9d39705896e8d9a74
Sergei Rud <sergejrud@gmail.com>
2022-03-05 18:44:13 +0300

    setup APP

--- RGB View/ViewController.swift
+++ RGB View/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redColorValue: UILabel!
+    @IBOutlet var greenColorValue: UILabel!
+    @IBOutlet var blueColorValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
+        
+        redColorValue.text = String(redSlider.value)
+        greenColorValue.text = String(greenSlider.value)
+        blueColorValue.text = String(blueSlider.value)
     }
-
-
+    
+    func changeColorView() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    @IBAction func redSliderAction() {
+        redColorValue.text = String(round(100 * redSlider.value) / 100)
+        changeColorView()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenColorValue.text = String(round(100 * greenSlider.value) / 100)
+        changeColorView()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueColorValue.text = String(round(100 * blueSlider.value) / 100)
+        changeColorView()
+    }
 }
-
