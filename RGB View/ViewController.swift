68fac229c08818f3f0b6f348077d6dbd9fe19f7b
Sergei Rud <sergejrud@gmail.com>
2022-03-05 11:24:15 +0300

    Initial Commit

---
+++ RGB View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGB View
+//
+//  Created by Сергей Рудь on 5.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
